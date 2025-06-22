eec78c2973b9d5a05baf518a1ca1799339fcf9ee
Akesha <akeshanawanjali23@gmail.com>
2025-06-22 09:26:20 +0530

    create product.js file

---
+++ models/products.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const productsSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+
+})
+
+const Products = mongoose.model("Products", productsSchema);
+export default Products;
