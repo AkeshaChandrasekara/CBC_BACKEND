605c93d5c2a72b60ed3e03d549374d47f25a92e7
Akesha <akeshanawanjali23@gmail.com>
2025-08-03 13:32:41 +0530

    update order controller and route🔶

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createOrder, getOrders, getQuote, updateOrder,createPaymentIntent,createCheckoutSession } from '../controllers/orderController.js';
+import { createOrder, getOrders, getQuote, updateOrder,createPaymentIntent} from '../controllers/orderController.js';
 
 const orderRouter = express.Router();
 
@@ -9,6 +9,6 @@ orderRouter.get("/", getOrders)
 orderRouter.post("/quote",getQuote)
 orderRouter.put("/:orderId",updateOrder)
 orderRouter.post("/create-payment-intent", createPaymentIntent);
-orderRouter.post("/create-checkout-session", createCheckoutSession);
+
 
 export default orderRouter;
