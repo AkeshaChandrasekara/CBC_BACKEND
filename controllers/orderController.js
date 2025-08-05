80b9342348ead3adc401349d91e696450e82f29f
Akesha <akeshanawanjali23@gmail.com>
2025-08-05 20:02:04 +0530

    update order controller for stock update🔶

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -7,34 +7,28 @@ import 'dotenv/config';
 const stripe = new Stripe(process.env.VITE_STRIPE_SECRET_KEY);
 
 export async function createOrder(req, res) {
-  if (!isCustomer) {
+  if (!isCustomer(req)) {
     res.json({
       message: "Please login as customer to create orders",
     });
+    return;
   }
 
   try {
     const latestOrder = await Order.find().sort({ orderId: -1 }).limit(1);
-    console.log(latestOrder);
-
     let orderId;
 
     if (latestOrder.length == 0) {
       orderId = "CBC0001";
     } else {
       const currentOrderId = latestOrder[0].orderId;
-
       const numberString = currentOrderId.replace("CBC", "");
-
       const number = parseInt(numberString);
-
       const newNumber = (number + 1).toString().padStart(4, "0");
-
       orderId = "CBC" + newNumber;
     }
 
     const newOrderData = req.body;
-
     const newProductArray = [];
 
     for (let i = 0; i < newOrderData.orderedItems.length; i++) {
@@ -43,36 +37,47 @@ export async function createOrder(req, res) {
       });
 
       if (product == null) {
-        res.json({
-          message:
-            "Product with id " +
-            newOrderData.orderedItems[i].productId +
-            " not found",
+        return res.json({
+          message: "Product with id " + newOrderData.orderedItems[i].productId + " not found",
         });
-        return;
       }
 
+      if (product.stock < newOrderData.orderedItems[i].qty) {
+        return res.json({
+          message: `Insufficient stock for product ${product.productName}. Only ${product.stock} available.`,
+        });
+      }
+    }
+    for (let i = 0; i < newOrderData.orderedItems.length; i++) {
+      const product = await Product.findOne({
+        productId: newOrderData.orderedItems[i].productId,
+      });
+
+      const newStock = product.stock - newOrderData.orderedItems[i].qty;
+      await Product.updateOne(
+        { productId: product.productId },
+        { stock: newStock }
+      );
+
       newProductArray[i] = {
+        productId: product.productId,
         name: product.productName,
         price: product.lastPrice,
         quantity: newOrderData.orderedItems[i].qty,
         image: product.images[0],
       };
     }
-    console.log(newProductArray);
 
     newOrderData.orderedItems = newProductArray;
-
     newOrderData.orderId = orderId;
     newOrderData.email = req.user.email;
 
     const order = new Order(newOrderData);
-
     const savedOrder = await order.save();
 
     res.json({
-      message: "Order created",
-      order : savedOrder
+      message: "Order created and stock updated",
+      order: savedOrder
     });
   } catch (error) {
     res.status(500).json({
